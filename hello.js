755a05f40a463be5656e80bb0ee325860ececab5
xc <xuc1124@gmail.com>
2016-08-16 13:35:15 +0800

    nodejs files

---
+++ hello.js
@@ -0,0 +1,12 @@
+'use strict';
+var s = 'Hello';
+function greet(name) {
+	console.log(s+','+name+'!');
+}
+function hello(){
+	console.log("Hello world!");
+}
+module.exports = {
+	greet:greet,
+	hello:hello
+}
